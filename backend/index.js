17823aff88a45cd936a72cdb72bd8e035df66d0e
Manuel Restrepo <manolo.1217@hotmail.com>
2021-08-09 14:31:53 -0500

    impÃlementacion de la carpeta controllers, routes y sus archivos

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const {dbConnection}= require("./db/db");
+const Role = require("./routes/role");
 require("dotenv").config();
 
 const app= express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/role",Role);
+
 
 app.listen(process.env.PORT,() => console.log("Backend server running OK, on port: ", process.env.PORT));
 
